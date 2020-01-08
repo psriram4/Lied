322f9b3fff0b313f3f5bf2b9cb01ba8e0303e21c
Pranav Sriram <47403620+psriram4@users.noreply.github.com>
2020-01-08 11:04:34 -0600

    modified to interact with web_app.py

--- graph.py
+++ graph.py
@@ -7,57 +7,37 @@ import spotipy
 import spotipy.util as util
 import spotipy.oauth2 as oauth2
 import pymongo
+from networkx.readwrite import json_graph
+import web_app
 
 '''
 Create an artist similarity graph and run bidirectional dijkstra's
 '''
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+def loadFromDatabase(collection_name, item_id):
+    bridge_db = web_app.mongo_client["bridge_db"]
+    collection = bridge_db[collection_name]
+    document = collection.find_one({ "_id": item_id})
+    return document
 
-spotipy_client_id = 'yayayeet'
-spotipy_client_secret = 'i like ganondorf'
-spotipy_redirect_uri = 'the john'
-username = 'cheeloy'
-scope = None
-
-# token = util.prompt_for_user_token(username, scope, client_id=spotipy_client_id, client_secret=spotipy_client_secret, redirect_uri=spotipy_redirect_uri)
-credentials = oauth2.SpotifyClientCredentials(client_id=spotipy_client_id, client_secret=spotipy_client_secret)
-token = credentials.get_access_token()
-
-if token:
-    spotify = spotipy.Spotify(auth=token)
-else:
-    print("can't get token for ", username)
-
-
-G = nx.Graph()
 
 def createGraph(data_col_name, track_data_col_name):
-
-    def loadFromDatabase(collection_name, item_id):
-        bridge_db = client["bridge_db"]
-        collection = bridge_db[collection_name]
-        document = collection.find_one({ "_id": item_id})
-        return document
-
+    web_app.G = nx.Graph()
 
     data = loadFromDatabase(data_col_name, 1)
     track_data = loadFromDatabase(track_data_col_name, 1)
-
-    # data = parseJSON(data_path)
-    # track_data = parseJSON(track_data_path)
 
     try:
         artists = data['artists']
     except data is None:
         status = 'Error in parsing data from path'
     for artist in artists:
-        G.add_node(artist['id'])
+        web_app.G.add_node(artist['id'])
         for neighbor in artist['related']:
-            if neighbor['id'] not in G.nodes:
-                G.add_node(neighbor['id'])
+            if neighbor['id'] not in web_app.G.nodes:
+                web_app.G.add_node(neighbor['id'])
 
-            if not G.has_edge(artist['id'], neighbor['id']):
+            if not web_app.G.has_edge(artist['id'], neighbor['id']):
                 # calculate edge weight
 
                 # this if statement will be taken out, some of the artists aren't in the dict cuz of the constraint
@@ -78,8 +58,20 @@ def createGraph(data_col_name, track_data_col_name):
                 G.add_edge(artist['id'], neighbor['id'], weight = edgeWeight)
 
 
-def minimizeTracks(artist_path, first_song, last_song):
+    bridge_db = web_app.mongo_client["bridge_db"]
+    graph_col = bridge_db["graph_col"]
 
+    graph_data = json_graph.node_link_data(G)
+    graph_json = {}
+    graph_json['_id'] = 1
+    graph_json['data'] = graph_data
+
+    inserted_graph = graph_col.insert_one(graph_json)
+    print(inserted_graph.inserted_id, " - graph data has been inserted")
+
+
+def minimizeTracks(artist_path, first_song, last_song):
+    
     tracks = []
     for i in range(len(artist_path)):
         if i == 0:
@@ -92,13 +84,13 @@ def minimizeTracks(artist_path, first_song, last_song):
             continue
 
         artist = artist_path[i]
-        top_tracks = spotify.artist_top_tracks(artist, country='US')
+        top_tracks = web_app.spotify.artist_top_tracks(artist, country='US')
         min_distance = 100000000
         next_track = None
 
-        curr_track_features = spotify.audio_features(curr_track)[0]
+        curr_track_features = web_app.spotify.audio_features(curr_track)[0]
         for top_track in top_tracks['tracks']:
-            next_track_features = spotify.audio_features(top_track['id'])[0]
+            next_track_features = web_app.spotify.audio_features(top_track['id'])[0]
             euclidean_distance = 0
             euclidean_distance += (curr_track_features['danceability'] - next_track_features['danceability'])**2
             euclidean_distance += (curr_track_features['energy'] - next_track_features['energy'])**2
@@ -116,34 +108,36 @@ def minimizeTracks(artist_path, first_song, last_song):
 
 
 def findShortestPath(start_song, end_song):
-    # print("start track")
-    start_artist = spotify.track(start_song)['artists'][0]['id']
-    end_artist = spotify.track(end_song)['artists'][0]['id']
+    start_artist = web_app.spotify.track(start_song)['artists'][0]['id']
+    end_artist = web_app.spotify.track(end_song)['artists'][0]['id']
 
     status = 'ok'
     try:
-        length, path = nx.bidirectional_dijkstra(G, start_artist, end_artist)
-    except nx.NetworkXNoPath:
+        length, path = nx.bidirectional_dijkstra(web_app.G, start_artist, end_artist)
+    except:
         status = 'No path between ' + start_song + ' and ' + end_song
-    return status,minimizeTracks(path, start_song, end_song)
 
-def cleanPathOutput(trackList):
-    output = []
-    for track in trackList:
-        output.append(spotify.track(track)['name'])
-    return output
+    tracks = minimizeTracks(path, start_song, end_song)
+
+    track_names = []
+    for track in tracks:
+        track_names.append(web_app.spotify.track(track)['name'])
+
+    return track_names
 
 
-if __name__ == '__main__':
-    # path = 'data.txt'
-    # song_path = 'tracks.txt'
-
+def getTrackList(start_song, end_song):
     data_col_name = "artist_col"
     tracks_col_name = "tracks_col"
-    createGraph(data_col_name, tracks_col_name)
-    start_song = '52VJwrmQflskeWoV0OmEEh'
-    end_song = '40bynawzslg9U7ACq07fAj'
-    status,track_list = findShortestPath(start_song, end_song)
 
-    for track in track_list:
-        print(spotify.track(track)['name'])
+    if "graph_col" in web_app.mongo_client["bridge_db"].list_collection_names():
+        graph_data = loadFromDatabase("graph_col", 1)
+        web_app.G = json_graph.node_link_graph(graph_data['data'])
+    else:
+        createGraph(data_col_name, tracks_col_name)
+
+    track_list = findShortestPath(start_song, end_song)
+    return track_list
+
+
+
