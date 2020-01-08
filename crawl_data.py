451997ffa03b719659de6641030a1c1da31f9acb
Pranav Sriram <47403620+psriram4@users.noreply.github.com>
2020-01-08 11:05:56 -0600

    added in pre-check if db exists

--- crawl_data.py
+++ crawl_data.py
@@ -7,10 +7,10 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-spotipy_client_id = 'a8a57761a69c4cadbc21223798fdf799'
-spotipy_client_secret = '2882513df3d24e46a89e034dd16ddf4f'
-spotipy_redirect_uri = 'http://localhost/'
-username = 'pranav.sriram'
+spotipy_client_id = 'king-nith'
+spotipy_client_secret = 'palutena-is-trash'
+spotipy_redirect_uri = 'pikachu is aids'
+username = 'wolf for the win'
 scope = None
 
 # token = util.prompt_for_user_token(username, scope, client_id=spotipy_client_id, client_secret=spotipy_client_secret, redirect_uri=spotipy_redirect_uri)
